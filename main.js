ac32a4e21903c7c73c333dab498c4b88272543ce
sebastianpiresmolin <piresmolinsecure@gmail.com>
2023-11-08 21:12:00 +0100

    add js logic for popinmenu, styling and animations

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
+document.getElementById("headerButton").addEventListener('click', function() {
+    let menuOverlay = document.getElementById("menuOverlay");
+    let popinMenu = document.getElementById("popinMenu");
 
+    menuOverlay.style.display = 'block'; // This shows my white overlay
+    popinMenu.style.display = 'flex'; // This shows my popin menu
+
+    setTimeout(function() {
+        menuOverlay.style.opacity = 1;
+        popinMenu.style.opacity = 1;
+    }, 10);
+
+    document.documentElement.style.overflowY = 'hidden'; // This disables the ability to scroll behind the menu
+});
+
+document.getElementById("closebtn").addEventListener('click', function() {
+    let menuOverlay = document.getElementById("menuOverlay");
+    let popinMenu = document.getElementById("popinMenu");
+
+    menuOverlay.style.opacity = 0;
+    popinMenu.style.opacity = 0;
+    
+    setTimeout(function() {
+        menuOverlay.style.display = 'none'; // This hides my white overlay
+        popinMenu.style.display = 'none'; // This hides my popin menu
+    }, 800);
+    
+    document.documentElement.style.overflowY = 'auto'; // This enables the ability to scroll again
+});
